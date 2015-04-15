395422f0f56e515375051e4c49cb79a35950478b
vinothmit <vinothkumr.10@gmail.com>
2015-04-15 10:45:33 +0800

    me

--- aboutme/vinothJs.js
+++ aboutme/vinothJs.js
@@ -1,20 +1,27 @@
-$(document).ready(function(){
-    $(document).scroll(function(){
-        var scrol = $(this).scrollTop();
-        var post = $('header').position();
-        if(scrol > post.top)
-        {
-            $('nav').css({"visibility":"hidden"});    
-$('#onmotion').css({"visibility":"initial","position":"fixed","top":"0","width":"100%","background-color": "rgb(20, 25, 30)",  "margin-top": "-50px"
-});
- $('#onmotion').animate({"margin-top": "0px"});
-         
-        }
-        else
-        {
-            $('nav').css({"visibility":"initial"});
-            $('#onmotion').css({"visibility":"hidden"});
-        }
-       
+$(document).ready(function() {
+    var a = !1;
+    $(document).scroll(function() {
+        var b = $(this).scrollTop(),
+            c = $("header").position();
+        b > c.top && !a ? ($("nav").css({
+            visibility: "hidden"
+        }), $("#onmotion").css({
+            visibility: "initial",
+            position: "fixed",
+            top: "0",
+            width: "100%",
+            "background-color": "rgb(20, 25, 30)",
+            "margin-top": "-50px"
+        }), $("#onmotion").animate({
+            "margin-top": "0px"
+        }), a = !0) : b < c.top && ($("#onmotion").animate({
+            "margin-top": "-50px"
+        }, function() {
+            $("nav").css({
+                visibility: "initial"
+            }), $("#onmotion").css({
+                visibility: "hidden"
+            })
+        }), a = !1)
     })
-})
+});
