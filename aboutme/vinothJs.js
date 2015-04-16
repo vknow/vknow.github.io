938606d8a82c6a0413057e97a0262b8359f4c870
vinothmit <vinothkumr.10@gmail.com>
2015-04-16 13:04:56 +0800

    me2

--- aboutme/vinothJs.js
+++ aboutme/vinothJs.js
@@ -1,27 +1,37 @@
 $(document).ready(function() {
-    var a = !1;
+    var flag = false;
     $(document).scroll(function() {
-        var b = $(this).scrollTop(),
-            c = $("header").position();
-        b > c.top && !a ? ($("nav").css({
-            visibility: "hidden"
-        }), $("#onmotion").css({
-            visibility: "initial",
-            position: "fixed",
-            top: "0",
-            width: "100%",
-            "background-color": "rgb(20, 25, 30)",
-            "margin-top": "-50px"
-        }), $("#onmotion").animate({
-            "margin-top": "0px"
-        }), a = !0) : b < c.top && ($("#onmotion").animate({
-            "margin-top": "-50px"
-        }, function() {
-            $("nav").css({
-                visibility: "initial"
-            }), $("#onmotion").css({
-                visibility: "hidden"
-            })
-        }), a = !1)
+        var scrol = $(this).scrollTop();
+        var post = $('header').position();
+
+        if (scrol > post.top && !flag) {
+            $('nav').css({
+                "visibility": "hidden"
+            });
+            $('#onmotion').css({
+                "visibility": "initial",
+                "position": "fixed",
+                "top": "0",
+                "width": "100%",
+                "background-color": "rgb(20, 25, 30)",
+                "margin-top": "-50px"
+            });
+            $('#onmotion').animate({
+                "margin-top": "0px"
+            });
+            flag = true;
+        } else   if(scrol <= post.top){
+            $('#onmotion').animate({
+                "margin-top": "-50px"
+            }, function() {
+                $('nav').css({
+                        "visibility": "initial"
+                    }),
+                    $('#onmotion').css({
+                        "visibility": "hidden"
+                    });
+            });
+            flag = false;
+        }
     })
-});
+})
