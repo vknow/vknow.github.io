3d2cc884c889a4e942493454a8cdf9d18dec62bd
vinothmit <vinothkumr.10@gmail.com>
2015-04-17 16:16:54 +0800

    till skill

--- aboutme/vinothJs.js
+++ aboutme/vinothJs.js
@@ -34,5 +34,14 @@ $(document).ready(function() {
             flag = false;
         }
     })
+    var value = document.getElementsByClassName('cut');
+    for (var i = 0; i < value.length; i++) {
+        value[i].style.webkitTransform = "rotate(" + value[i].getAttribute('data-rot') + "deg)";
+        value[i].style.MozTransform = "rotate(" + value[i].getAttribute('data-rot') + "deg)";
+        value[i].style.msTransform = "rotate(" + value[i].getAttribute('data-rot') + "deg)";
+        value[i].style.OTransform = "rotate(" + value[i].getAttribute('data-rot') + "deg)";
+        value[i].style.transform = "rotate(" + value[i].getAttribute('data-rot') + "deg)";
+    };
 });
+
 // $(document).ready(function(){var i=!1;$(document).scroll(function(){var o=$(this).scrollTop(),n=$("header").position();o>n.top&&!i?($("nav").css({visibility:"hidden"}),$("#onmotion").css({visibility:"initial",position:"fixed",top:"0",width:"100%","background-color":"rgb(20, 25, 30)","margin-top":"-50px"}),$("#onmotion").animate({"margin-top":"0px"}),i=!0):o<=n.top&&($("#onmotion").animate({"margin-top":"-50px"},function(){$("nav").css({visibility:"initial"}),$("#onmotion").css({visibility:"hidden"})}),i=!1)})});
